1b9dd8d22f2c623f821a278d211b3b8d84bafa44
ShinHoCheol <penclub79@naver.com>
2022-12-13 00:03:55 +0900

    codeup Practice 100

---
+++ codeUp/Practice100/Practice100/34scanf.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char c = NULL;
+	
+	scanf("%c", &c);
+
+	printf("%d", c);
+
+
+	return 0;
+}
---
+++ codeUp/Practice100/Practice100/35scanf.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	printf("%c", i);
+
+	return 0;
+}
