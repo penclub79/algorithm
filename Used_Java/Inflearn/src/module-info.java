fce9bde4d0928a141001e327284f2f683622f4dd
ShinHoCheol <https://github.com/penclub79>
2023-04-27 19:50:42 +0900

    Commit

---
+++ Used_Java/Inflearn/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Shin
+ *
+ */
+module Note {
+}
---
+++ Used_Java/Inflearn/src/practiceWork/Note.java
@@ -0,0 +1,25 @@
+package practiceWork;
+
+import java.util.Scanner;
+
+public class Note {
+	
+	public int solution(String str, char t)
+	{
+		int answer = 0;
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		int input1 = in.nextInt();
+		int input2 = in.nextInt();
+		
+		System.out.println(solution());
+		return;
+	}
+
+}
