8a1d08faf0c6a5c4ddd6bce33c84a634d9748556
ShinHoCheol <penclub79@naver.com>
2023-05-17 23:14:11 +0900

    Commit

--- Used_Java/Inflearn/src/practiceWork/Note.java
+++ Used_Java/Inflearn/src/practiceWork/Note.java
@@ -4,23 +4,6 @@ import java.util.Scanner;
 
 public class Note {
 
-	public static int solution(int n)
-	{
-		int answer = 0;
-		int cnt = 0;
-		int totalCnt = 0;
-
-		for (int i = 2; i <= n; i++)
-		{
-			if (n % i == 0)
-				continue;
-			else
-				answer++;
-		}
-
-		return answer;
-	}
-
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
